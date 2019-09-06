b708e68e12a35dab39479d2e27054f339d56af60
red_angel <zhe.73@bk.ru>
2019-09-06 19:53:46 +0400

    Updated Main class

--- src/main/java/ru/kruglov/task1/Main.java
+++ src/main/java/ru/kruglov/task1/Main.java
@@ -2,9 +2,12 @@ package ru.kruglov.task1;
 
 public class Main {
     public static void main(String Args[]) {
-        GasolineCounter gc = new GasolineCounter(100);
-        ClearSalaryCounter tc = new ClearSalaryCounter(20000);
-        TimeConverter timeConverter = new TimeConverter(0);
+        GasolineCounter gasolineCounter = new GasolineCounter(100);
+        ClearSalaryCounter salaryCounter = new ClearSalaryCounter(20000);
+        TimeConverter timeConverter = new TimeConverter(8900);
+
+        System.out.println(gasolineCounter.getGasolineCost());
+        System.out.println(salaryCounter.getClearSalary());
         System.out.println(timeConverter.getTimeInHours());
     }
-    }
+}
