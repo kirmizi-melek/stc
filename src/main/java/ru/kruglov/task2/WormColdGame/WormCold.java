7969549cce74d592ad227fd022088265d3badb44
red_angel <zhe.73@bk.ru>
2019-09-07 22:03:48 +0400

    Completed version of WarmColdGame

--- src/main/java/ru/kruglov/task2/WormColdGame/WormCold.java
+++ src/main/java/ru/kruglov/task2/WormColdGame/WormCold.java
@@ -1,12 +1,10 @@
 package ru.kruglov.task2.WormColdGame;
 
-import javax.sound.midi.Soundbank;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class WormCold {
-
 
     private String getDataFromSystemIn() throws IOException {
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
@@ -21,12 +19,17 @@ public class WormCold {
     }
 
     private String callbackOnPlayerAnswer(int guessedNumeric, int currentPlayerGuess, int previousPlayerGuess) {
-        //String rightAnswer = "Right answer. Congragulation!";
-        //String answerIsLess = "Wo ";
-
-        if ((guessedNumeric-currentPlayerGuess) > (guessedNumeric-previousPlayerGuess)) {
-            return "Warmer";
-        } else return "Colder";
+        int diffBetweenGuessedAndCurrent = (guessedNumeric - currentPlayerGuess);
+        int diffBetweenGuessedAndPrevious = (guessedNumeric - previousPlayerGuess);
+        if (currentPlayerGuess < guessedNumeric) {
+            if (diffBetweenGuessedAndPrevious > diffBetweenGuessedAndCurrent) {
+                return "Warmer\n";
+            } else return "Colder\n";
+        } else {
+            if (diffBetweenGuessedAndCurrent > diffBetweenGuessedAndPrevious) {
+                return "Warmer\n";
+            } else return "Colder\n";
+        }
     }
 
     private int numericRandomizer() {
@@ -39,13 +42,26 @@ public class WormCold {
         int currentPlayerGuess = -1;
         int previousPlayerGuess = 0;
         int guessedNumeric = numericRandomizer();
-        System.out.println("Welcome to the Cold Warm Game! Please type any numeric from 0 to 100");
-        System.out.println("Guessed numeric = " + guessedNumeric);
-        while (currentPlayerGuess != guessedNumeric) {
+        System.out.println("Welcome to the Cold Warm Game! \nPlease type any numeric from 0 to 100\n");
+        //System.out.println("Guessed numeric = " + guessedNumeric);
+        while (!(numericComparator(guessedNumeric, currentPlayerGuess))) {
             System.out.println("Your answer:");
-            currentPlayerGuess = Integer.parseInt(getDataFromSystemIn());
-            System.out.println(
-                    callbackOnPlayerAnswer(guessedNumeric, currentPlayerGuess, previousPlayerGuess));
+            String playerInputPhrase = getDataFromSystemIn();
+            if (playerInputPhrase.equals("exit")) {
+                System.out.println("Bye-bye");
+                System.exit(0);
+            } else if (playerInputPhrase.equals("help")) {
+                System.out.println("\nApplication commands\n" +
+                        "help - get help about the game\n" +
+                        "exit - quit the game\n");
+            } else {
+                currentPlayerGuess = Integer.parseInt(playerInputPhrase);
+                if (!(numericComparator(guessedNumeric, currentPlayerGuess))) {
+                    System.out.println(
+                            callbackOnPlayerAnswer(guessedNumeric, currentPlayerGuess, previousPlayerGuess));
+                } else System.out.println("You're won! Congragulation!");
+                previousPlayerGuess = currentPlayerGuess;
+            }
         }
     }
 
@@ -57,6 +73,5 @@ public class WormCold {
             ioe.printStackTrace();
             System.exit(1);
         }
-
     }
 }
