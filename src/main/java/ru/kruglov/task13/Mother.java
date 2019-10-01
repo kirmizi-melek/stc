d753334acf8a6bba66da58eac74c63b74fc4af08
red_angel <zhe.73@bk.ru>
2019-10-01 19:51:31 +0400

    Some minor changes

--- src/main/java/ru/kruglov/task13/Mother.java
+++ src/main/java/ru/kruglov/task13/Mother.java
@@ -4,12 +4,13 @@ public class Mother {
     public static void main(String[] args) {
         Mother anna = new Mother();
         Child ivan = new Child();
-        anna.feed(ivan);
+        Food food = Food.PORRIGE;
+        anna.feed(ivan, food);
     }
 
-    private void feed(Child child){
+    private void feed(Child child, Food food){
         try {
-            child.eat(Food.PORRIGE);
+            child.eat(food);
         } catch (ChildFeedException e) {
             e.printStackTrace();
         } finally {
