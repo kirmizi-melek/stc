cc4d126c8117c983ea4734ad8607f11bfaece8c9
red_angel <zhe.73@bk.ru>
2019-11-04 17:56:05 +0400

    added comments to main method

--- src/main/java/ru/kruglov/task25/Main.java
+++ src/main/java/ru/kruglov/task25/Main.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        // task 25.1 Filtering of Map.
+        // If duplicate values are exist - return FALSE, else - TRUE
         HashMap<String,String> hmp = new HashMapPreparator().hashMapPreparator();
         System.out.println(new DuplicateValueFinder().hashMapDuplicateValueFinder(hmp));
     }
