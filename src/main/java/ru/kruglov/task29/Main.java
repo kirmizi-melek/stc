9828e529995158f96b6f106a2ae804ba12154d4c
red_angel <zhe.73@bk.ru>
2019-11-16 12:20:21 +0400

    Added comments to main() method

--- src/main/java/ru/kruglov/task29/Main.java
+++ src/main/java/ru/kruglov/task29/Main.java
@@ -3,16 +3,25 @@ package ru.kruglov.task29;
 public class Main {
 
     public static void main(String[] args) {
+        //Creating of hasmap
         PersonHashMapCreator phmc = new PersonHashMapCreator();
+
+        //Printing hashMap in console
         System.out.println("hashMap before deleting duplicates");
         PersonHashMapActions.printOutputHashMap(phmc.getHashMap());
+
+        //Finding and deleting of duplicates from hashmap
         System.out.println("\nhashMap after deleting duplicates");
         PersonHashMapActions.removeTheDuplicates(phmc.getHashMap());
+
+        //Printing of hashMap without duplicates in console
         PersonHashMapActions.printOutputHashMap(phmc.getHashMap());
 
+        //Deleting of element from hashMap by separate request
         PersonHashMapActions.removeItemFromMapByValue(phmc.getHashMap(), new Person(1,"Maria", 'F'));
+
+        //Printing of hashMap after deleting of single element
         System.out.println("\nhashMap after deleting item by its value");
         PersonHashMapActions.printOutputHashMap(phmc.getHashMap());
-
     }
 }
